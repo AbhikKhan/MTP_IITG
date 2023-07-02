25546c2b9bb3c3d93f28daa9dff1d46c878fa864
Abhik Khan <abhik.khan.1997@gmail.com>
2023-07-03 00:33:53 +0530

    Generated possible test cases of 500 size in /TestCases

--- TestCases/generateAll.py
+++ TestCases/generateAll.py
@@ -7,20 +7,20 @@ def get_all_possible_values(number_of_variables, target_sum, output_file):
     solver = Solver()
 
     # Create Z3 variables for x1, x2, x3, and x4
-    x = []
+    R = []
     for i in range(0, number_of_variables):
-        var_name = f'x{i+1}'
+        var_name = f'R{i+1}'
         var = Int(var_name)
         solver.add(var > 0)
-        x.append(var)
+        R.append(var)
 
     # Adding constraint
-    solver.add(Sum(x) == target_sum)
+    solver.add(Sum(R) == target_sum)
 
     with open(output_file, 'w', newline='') as csvfile:
         writer = csv.writer(csvfile)
 
-        header = [f'x{i+1}' for i in range(number_of_variables)]
+        header = [f'R{i+1}' for i in range(number_of_variables)]
         writer.writerow(header)
 
         iter = 500
@@ -31,19 +31,19 @@ def get_all_possible_values(number_of_variables, target_sum, output_file):
             iter = iter-1
             model = solver.model()
 
-            values = [model.eval(var).as_long() for var in x]
+            values = [model.eval(var).as_long() for var in R]
 
             # write in CSV file
             writer.writerow(values)
 
             # Print the values dynamically based on the number of variables
-            # variables_str = ', '.join(f'x{i+1} = {val}' for i, val in enumerate(values))
-            # print(variables_str)
+            variables_str = ', '.join(f'R{i+1} = {val}' for i, val in enumerate(values))
+            print(variables_str)
 
             # Add constraint to exclude the current solution and it's permutations
             allPermuations = list(permutations(values))
             for per in allPermuations:
-                constraint = Or(*[var != val for var, val in zip(x, per)])
+                constraint = Or(*[var != val for var, val in zip(R, per)])
                 solver.add(constraint)
 
 # Get and print all possible values
